936d289c9548caea29dce8b9e9b3d7702f0ad58b
Tomer Elmalem <telmalem@gmail.com>
2011-12-04 14:00:56 -0500

    Add a simple test of allocating memory and then copying a string over.

--- test.c
+++ test.c
@@ -1,4 +1,6 @@
 #include "my_malloc.h"
+#include <string.h>
+#include <stdio.h>
 
 int main() {
 	
@@ -6,5 +8,10 @@ int main() {
 	 * Test code goes here
 	 */
 	 
+        char *a;
+        char b[] = "hello peepz";
+        a = my_malloc(sizeof(char)*20);
+        strncpy(a, b, 20);
+
 	return 0;
 }
