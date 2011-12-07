8c10d50668fc9e0a4f825db7294efec09b7af0dc
Tomer Elmalem <telmalem@gmail.com>
2011-12-07 15:53:50 -0500

    Make sure to call my_malloc for the correct size.

--- test.c
+++ test.c
@@ -10,6 +10,7 @@ int main() {
 	 
         char *a;
         char b[] = "hello peepz";
+        printf("String of size %d:\n",strlen(b));
         a = my_malloc(sizeof(char)*strlen(b));
         strncpy(a, b, strlen(b));
 
@@ -20,6 +21,7 @@ int main() {
 
         char *c;
         char d[] = "iz thiz workingz?";
+        printf("String of size %d:\n",strlen(b));
         c = my_malloc(sizeof(char)*strlen(d));
         strncpy(c, d, strlen(d));
 
@@ -27,7 +29,8 @@ int main() {
         printf("%s\n", d);
 
         char e[] = "10 Home\n20 Sweet\n30 GOTO 10";
-        c = my_realloc(c, sizeof(char)*strlen(d));
+        printf("String of size %d:\n",strlen(e));
+        c = my_realloc(c, sizeof(char)*strlen(e));
         strncpy(c, e, strlen(e));
 
         printf("%s\n", c);
